91a8dedefba5094c9565cd992285556169b5dd25
Anav <anavroy@gmail.com>
2024-06-11 10:47:11 +0530

    added comments

--- project1/views.py
+++ project1/views.py
@@ -10,6 +10,8 @@ def say(request):
 def uploads(request):
     if request.method == 'POST' and request.FILES['pdf-upload']:
         pdf_file = request.FILES['pdf-upload']
+        
+        #saveing uploaded file in sytem storage
         fs = FileSystemStorage()
         filename = fs.save(pdf_file.name, pdf_file)
         uploaded_file_path = fs.path(filename)
@@ -21,6 +23,7 @@ def uploads(request):
         # Generate URLs for the split files
     
         split_file_urls =   [request.build_absolute_uri(settings.MEDIA_URL + os.path.basename(file)) for file in output_files]
+        
         return render(request, 'template/uploads.html', {'split_file_urls': split_file_urls})
     return render(request, 'template/uploads.html')
 
@@ -33,14 +36,19 @@ def split_pdfs(input_file_path):
     name, ext = os.path.splitext(base_filename)
 
     output_files = []
+    
     for i, page in enumerate(inputpdf.pages):
         output = PdfWriter()
         output.add_page(page)
-
+        
+        #Generating file path for the split files
         out_file_path = os.path.join(output_path, f"{name}_page_{i+1}.pdf")
+        
+        #writing contents of Pdfwriter object to the new output files
         with open(out_file_path, "wb") as output_stream:
             output.write(output_stream)
 
+        #Appending path of output files to output_files list
         output_files.append(out_file_path)
 
     return output_files
